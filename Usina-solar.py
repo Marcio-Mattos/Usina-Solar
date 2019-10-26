98e9f64cf4d93319fb91e3ebe8665090b894b1cc
marcio-mattos <marcioamattos@poli.ufrj.br>
2019-10-26 11:51:56 -0300

    Iniciando dimensionamento

--- Usina-solar.py
+++ Usina-solar.py
@@ -1,3 +1,66 @@
-# Vamos começar os trabalhos.
+# Dimensionamento de um sistema votofoltaico.
 
-print('Vamos começar o dimensionamento.')
+# Em função do consumo médio residencial, dimensionar a potência necessária
+
+# a ser instalada.
+
+print('*'*40)
+
+print('     Dimensionamento Sistema Solar')
+
+print('*'*40)
+
+print()
+
+projeto = {}
+
+projetos = []
+
+
+while True:
+
+    projeto.clear()
+    
+    projeto['cliente'] = input('Nome do cliente: ')
+
+    projeto['endereco'] = input('Endereço: ')
+
+    projeto['resptec'] = input('Responsál técnico: ')
+
+    projeto['medpotmensal'] = float(input('Informe a média do consumo mensal em KWh: '))
+
+    hsolpleno = float(input('Informe as horas de sol pleno: '))
+
+    potdia = projeto['medpotmensal']/24
+
+    projeto['potinstal'] = potdia/hsolpleno
+
+    projetos.append(projeto.copy())
+
+    while True:
+
+        resp = input('Deseja continuar? [S/N]: ').upper()[0]
+
+        if resp in 'SN':
+
+            break
+
+        print('ERRO ! Digite [S] ou [N].')
+
+    if resp == 'N':
+
+        break
+
+print('*'*40)
+
+print(projetos)
+
+print('*'*40)
+
+print('<<< FIM >>>')
+
+    
+
+
+
+
